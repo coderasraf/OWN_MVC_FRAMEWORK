8fde13473d64df243c7a22ebd861050a3516bd89
hassasraf <hassasraf@gmail.com>
2021-06-09 15:39:39 +0600

    Initial git commit

--- index.php
+++ index.php
@@ -3,18 +3,38 @@
 <?php include "system/libs/DController.php"; ?>
 <?php 
 
-		$url = $_GET['url'];
+	// Getting controller, method, param from url
+	$url = isset ($_GET['url']) ? $_GET['url'] : NULL;
+	if ($url != NULL) {
 		$url = rtrim($url, '/');
 		$url = explode('/', $url);
-		
+	}else{
+		unset($url);
+	}
+
+	// controllr, method, and param define
+	if (isset($url[0])){
 		include "app/controllers/$url[0].php";
 		$ctlr   = new $url[0]();
-		$method = $url[1];
-		$param  = $url[2];
-		echo "<br>";
-		$ctlr->$method($param);
+		if (isset($url[2]) && isset($url[1])){
+			$method = $url[1];
+			$param  = $url[2];
+			$ctlr->$method($param);
+		}
+		else{
+			if (isset($method)) {
+				$ctlr->$method();
+			}
+		}
+	}else{
+		echo "Defualt";
+	}
 
 
- ?>
 
-<?php include "inc/footer.php"; ?>
+
+
+
+
+// including footer file
+include "inc/footer.php";
